41525c9c451570e3cc24c3906de404007050dc68
Nguyen Tuan Anh <hamsterviel01@gmail.com>
2025-03-26 15:06:25 +0800

    finally successful

--- common_func.go
+++ common_func.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"math"
+	"math/rand"
 	"os"
 	"slices"
+	"sort"
 
 	"github.com/gocarina/gocsv"
 	log "github.com/sirupsen/logrus"
@@ -16,6 +19,19 @@ type MatchMetadata struct {
 	Player3              string  `csv:"player3"`
 	Player4              string  `csv:"player4"`
 	PercentageDifference float64 `csv:"percentage_difference"`
+}
+
+type MatchMakerTeam struct {
+	Player1 string
+	Player2 string
+}
+
+type PlayerMetadata struct {
+	Name        string  `csv:"name"`
+	Gender      string  `csv:"gender"`
+	TitRanking  float64 `csv:"tit_ranking"`
+	TaRanking   float64 `csv:"ta_ranking"`
+	MinhRanking float64 `csv:"minh_ranking"`
 }
 
 func loadAvgRankingAndGender() (map[string]float64, map[string]string, error) {
@@ -44,6 +60,26 @@ func loadAvgRankingAndGender() (map[string]float64, map[string]string, error) {
 		playerGender[player.Name] = player.Gender
 	}
 	return playerAvgRanking, playerGender, nil
+}
+
+func sortPlayersByRanking() []PlayerMetadata {
+	// Load from csv and take average of all ranking score. also output average team ranking
+	rankingFile, err := os.OpenFile("ranking_score.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	defer rankingFile.Close()
+
+	playersMetadatas := []PlayerMetadata{}
+	if err := gocsv.UnmarshalFile(rankingFile, &playersMetadatas); err != nil {
+		// Load clients from file
+		panic(err)
+	}
+
+	sort.Slice(playersMetadatas, func(i, j int) bool {
+		return playersMetadatas[i].MinhRanking + playersMetadatas[i].TaRanking + playersMetadatas[i].TitRanking > playersMetadatas[j].MinhRanking + playersMetadatas[j].TaRanking + playersMetadatas[j].TitRanking
+	})
+	return playersMetadatas
 }
 
 func isAllPlayersDifferentAndNoTwoFemaleSameTeam(players []string, playerGender map[string]string) bool {
@@ -80,4 +116,50 @@ func percentageDifference(player1, player2, player3, player4 string, playerAndRa
 	rankOfLesserTeam := math.Min(playerAndRanking[player1]+playerAndRanking[player2], playerAndRanking[player3]+playerAndRanking[player4])
 
 	return rankDistance / rankOfLesserTeam
+}
+
+func assignMatchesToCourts(matchMakerMatches []MatchMetadata) ([]MatchMetadata, error) {
+	// Randomly shuffle the matches order
+	for i := range matchMakerMatches {
+		j := rand.Intn(i + 1)
+		matchMakerMatches[i], matchMakerMatches[j] = matchMakerMatches[j], matchMakerMatches[i]
+	}
+
+	playersInCurrentRound := []string{}
+	courtNo := 1
+	for i := range matchMakerMatches {
+		if courtNo == 0 {
+			courtNo = 4
+		}
+
+		if courtNo == 1 {
+			playersInCurrentRound = []string{}
+		}
+
+		// If any of 4 player already play this round, find closest group of 4 players that hasn't play and swap the index
+		if isPlayerExistInList(playersInCurrentRound, matchMakerMatches[i]) {
+			foundMatch := false
+			for j := i + 1; j < len(matchMakerMatches); j++ {
+				if !isPlayerExistInList(playersInCurrentRound, matchMakerMatches[j]) {
+					matchMakerMatches[i], matchMakerMatches[j] = matchMakerMatches[j], matchMakerMatches[i]
+					foundMatch = true
+					break
+				}
+			}
+			if !foundMatch {
+				err := fmt.Errorf("cannot allocate court for some reason, playersInCurrentRound = %v, court %d remaining matches = %v", playersInCurrentRound, courtNo, matchMakerMatches[i+1:])
+				log.Warn(err)
+				matchMakerMatches[i].Court = 4
+				playersInCurrentRound = append(playersInCurrentRound, matchMakerMatches[i].Player1, matchMakerMatches[i].Player2, matchMakerMatches[i].Player3, matchMakerMatches[i].Player4)
+				courtNo = 1
+				continue
+			}
+		}
+
+		matchMakerMatches[i].Court = courtNo
+		playersInCurrentRound = append(playersInCurrentRound, matchMakerMatches[i].Player1, matchMakerMatches[i].Player2, matchMakerMatches[i].Player3, matchMakerMatches[i].Player4)
+		courtNo = (courtNo + 1)%4
+	}
+
+	return matchMakerMatches, nil
 }
--- match_maker_test.go
+++ match_maker_test.go
@@ -6,7 +6,7 @@ import (
 
 func Test_generateMatchMakerMatchesUntilSuccess(t *testing.T) {
 	tests := []struct {
-		name    string
+		name string
 	}{
 		{
 			"TestWithCsv",
@@ -20,6 +20,26 @@ func Test_generateMatchMakerMatchesUntilSuccess(t *testing.T) {
 				return
 			}
 			t.Error("Test actually success, just want to print out logs")
+		})
+	}
+}
+
+func Test_teamAssign(t *testing.T) {
+	tests := []struct {
+		name    string
+	}{
+		{
+			"TestWithCSV",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := teamAssign()
+			if err != nil {
+				t.Errorf("teamAssign() error = %v", err)
+				return
+			}
+			t.Error("test success, just want to see result")
 		})
 	}
 }
